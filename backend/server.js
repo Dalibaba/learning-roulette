749c6839c49fb8272999a81ae6a498a1ca60f364
Dalibaba <linkedavi@googlemail.com>
2021-09-13 18:25:55 +0200

    mongodb connected

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { isObject } = require("util");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
 const app = express()
 //create server based on express server
 const server = require('http').Server(app)
@@ -7,6 +8,8 @@ const server = require('http').Server(app)
 const io = require('socket.io')(server)
 // create individual uuid
 const {v4: uuidV4} = require('uuid')
+//dotenv
+dotenv.config();
 
 //view engine
 app.set('view engine', 'ejs')
@@ -17,7 +20,6 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
     //create dynamic url for room
     res.redirect(`/${uuidV4()}`)
-
 }
 )
 
@@ -43,5 +45,18 @@ io.on('connection', socket => {
     }
     )
 })
+
+
+mongoose.connect("mongodb://localhost:27017/learning-roulette", {
+}).then(()=> console.log("MongoDB Connected"))
+.catch((err => console.log(err)));
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    /* eslint-disable-next-line */
+    console.log('Successfully connected to database!');
+  });
+  
 
 server.listen(3000)
